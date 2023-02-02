a2ac68845559e3f4132f897e32e5282a1620aea1
FreakyCoder <kurayogun@gmail.com>
2023-02-02 20:53:22 +0300

    feat: adapty 2.3.0

--- App.tsx
+++ App.tsx
@@ -1,20 +1,83 @@
 import React, {useEffect} from 'react';
-import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
-import {adapty} from 'react-native-adapty';
+import {View, Text, StyleSheet, TouchableOpacity, Alert} from 'react-native';
+import {adapty, AdaptyError} from 'react-native-adapty';
 import useIAP from './src/useIAP';
 
-const App: React.FC = () => {
-  useEffect(() => {
-    (async () => {
-      try {
-        await adapty.activate('your-api-key');
-      } catch (e) {
-        console.log({e});
-      }
-    })();
-  }, []);
+async function init() {
+  try {
+    console.info('[ADAPTY] Activating Adapty SDK...');
+    // Async activate Adapty
+    await adapty.activate('TOKEN', {
+      lockMethodsUntilReady: true,
+    });
+  } catch (error: any) {
+    console.error('[ADAPTY] Error activating Adapty SDK', error.message);
+  }
+}
+init();
 
+const App: React.FC = () => {
   const {isPremiumActive} = useIAP();
+
+  const fetchProfile = async (shouldAlert = false) => {
+    // setIsLoadingProfile(true);
+
+    try {
+      console.info('[ADAPTY] Fetching user profile...');
+      const _profile = await adapty.getProfile();
+      console.log('Profile: ', _profile);
+      // setProfile(_profile);
+    } catch (error: any) {
+      console.error('[ADAPTY] Error fetching user profile', error.message);
+
+      if (shouldAlert && error instanceof AdaptyError) {
+        Alert.alert(
+          `Error fetching user profile ${error.adaptyCode}`,
+          error.localizedDescription,
+        );
+      }
+    } finally {
+      // setIsLoadingProfile(false);
+    }
+  };
+
+  useEffect(() => {
+    async function fetch() {
+      try {
+        console.info('[ADAPTY] Activating Adapty SDK...');
+        // Async activate Adapty
+        await adapty.activate('TOKEN', {
+          lockMethodsUntilReady: true,
+        });
+      } catch (error: any) {
+        console.error('[ADAPTY] Error activating Adapty SDK', error.message);
+      }
+
+      try {
+        // Set fallback paywalls
+        // const json = await Platform.select({
+        //   ios: import('./assets/ios_fallback.json'),
+        //   android: import('./assets/android_fallback.json'),
+        // });
+        // const str = JSON.stringify(json); // Webpack converts json string to object, but we need string here
+        // await adapty.setFallbackPaywalls(str);
+      } catch {}
+
+      fetchProfile();
+
+      adapty.addEventListener('onLatestProfileLoad', profile_ => {
+        console.info('[ADAPTY] onLatestProfileLoad', profile_);
+        // setProfile(profile_);
+      });
+    }
+    fetch();
+
+    return () => {
+      // Unsubscribe from adapty events
+      console.log('[ADAPTY] Unsubscribing from adapty events');
+      adapty.removeAllListeners();
+    };
+  }, []);
 
   const handleCheckPremium = () => {
     isPremiumActive();
